583a7aac643202615efb5bd640d4687ae712f378
Angsumi <angsuman@example.com>
2026-08-15 13:58:27 +0530

    fix: Normalize robots.txt sitemap URL to lowercase domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/dashboard/'],
     },
-    sitemap: 'https://Angsumi.github.io/ADRE/sitemap.xml',
+    sitemap: 'https://angsumi.github.io/ADRE/sitemap.xml',
   };
 }
